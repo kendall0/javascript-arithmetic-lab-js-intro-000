edc542607f6fb0691aac56dd4cde55eb57b042a7
Kendall George <kendallanngeorge@gmail.com>
2021-02-25 22:32:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,20 @@ function divideBy4() {
 
 divideBy3()
 
-console.log(number)
+console.log(number) // / 3.333333333335
 
 add5()
+
+console.log(number) // 8.333333333335
+
+//reset number
+number = 10
+
+add5()
+
+console.log(number) // 15
+
+divideBy3()
+
+console.log(number) // 5
+
