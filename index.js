1a3a7971243037cee66eedb4ec6ab2e85171eaa5
Kendall George <kendallanngeorge@gmail.com>
2021-02-25 22:23:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,18 +16,3 @@ function multiply(x, y) {
 function divide(x, y) {
   console.log(x / y);
 }
-
-var number = 10
-function add5() {
-  number += 5
-}
-
-function divideby3() {
-  number /= 3
-}
-
-divideby3()
-
-console.log(number)
-
-add5
